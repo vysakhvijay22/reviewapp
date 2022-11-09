437b80bc13d1522c087761f04d038baf238bc60c
vysakhvijay22 <vysakhvijay22@gmail.com>
2022-11-09 22:05:50 +0530

    this is 1st comment

--- flask_app.py
+++ flask_app.py
@@ -84,6 +84,6 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000)
     #app.run(host='0.0.0.0', port=port)
-    #app.run(host='127.0.0.1', port=8001, debug=True)
+    app.run(host='127.0.0.1', port=8001, debug=True)
